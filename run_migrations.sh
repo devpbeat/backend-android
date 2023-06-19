9bbd0a32309ec9e6b8f2cabdad65b57c5ed8b07c
Nahuel Pereira <nahuel.pereira@upacifico.edu.py>
2023-06-19 14:42:51 -0400

    main

--- run_migrations.sh
+++ run_migrations.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#/bin/bash
 # make script to run migrations from a container called 'web'
 # Usage: ./run_migrations.sh
-docker-compose run web python manage.py makemigrationss
-docker-compose run web python manage.py migrate
-
+docker-compose run web python manage.py makemigrations --noinput
+docker-compose run web python manage.py migrate --noinput
