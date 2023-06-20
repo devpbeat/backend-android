5b80765440594f2d0330200d0baf996a515d73f7
Nahuel Pereira <yo@devpbeat.rocks>
2023-06-19 21:35:49 -0400

    main

--- patient/serializers.py
+++ patient/serializers.py
@@ -11,22 +11,35 @@ class PatientSerializer(serializers.ModelSerializer):
 
     
     def create(self, validated_data):
+
+        user_ = User.objects.filter(username=validated_data['email']).exists()
         # Create patient user
-        user = User.objects.create_user(
-            username=validated_data['email'],
-            password=validated_data['password'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
-        )
-        validated_data.pop('password')
-        validated_data['user'] = user
-        patient = Patient.objects.create(**validated_data)
-        return patient
+        if not user_:
+            user = User.objects.create_user(
+                username=validated_data['email'],
+                password=validated_data['password'],
+                email=validated_data['email'],
+                first_name=validated_data['first_name'],
+                last_name=validated_data['last_name']
+            )
+            user.is_active = True
+            user.is_staff = True
+            user.is_superuser = False
+            user.save()
+            validated_data.pop('password')
+            validated_data['user'] = user
+            patient = Patient.objects.create(**validated_data)
+            return patient
+        else:
+            raise serializers.ValidationError({"email": "Email already exists"})
     
     def update(self, instance, validated_data):
-        patient = super().update(instance, validated_data)
-        return patient
-    
+        # Update patient user
+        user_ = User.objects.filter(username=validated_data['email']).exists()
+        if not user_:
+            patient = super().update(instance, validated_data)
+            return patient
+        else:
+            raise serializers.ValidationError({"email": "Email already exists"})
     
 
